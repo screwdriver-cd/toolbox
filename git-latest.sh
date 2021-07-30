57d575ada7dab30a076377cd5ec3ed3e7f972d7a
Jithin Emmanuel <jithine@verizonmedia.com>
2021-07-30 12:45:31 -0700

    fix: make script compatible with docker container wget (#25)

--- git-latest.sh
+++ git-latest.sh
@@ -3,9 +3,8 @@
 GIT_VERSION=/tmp/gitversion
 if [ ! -f "$GIT_VERSION" ] ; then
     echo Downloading gitversion
-    wget -q -O - https://github.com/screwdriver-cd/gitversion/releases/latest \
-       | egrep -o '/screwdriver-cd/gitversion/releases/download/v[0-9.]*/gitversion_linux_amd64' \
-       | wget --base=http://github.com/ -i - -O /tmp/gitversion
+    GIT_URI_PATH=$(wget -q -O - https://github.com/screwdriver-cd/gitversion/releases/latest | egrep -o '/screwdriver-cd/gitversion/releases/download/v[0-9.]*/gitversion_linux_amd64')
+    wget -O $GIT_VERSION http://github.com/$GIT_URI_PATH
     chmod +x $GIT_VERSION
 fi
 
