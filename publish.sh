f32a71b5264397c76d193c1b9cbb539d28bf5fe0
Jithin Emmanuel <jithine@verizonmedia.com>
2021-08-03 10:22:28 -0700

    fix: get previous behavior back, so that linux environment can work

--- publish.sh
+++ publish.sh
@@ -25,7 +25,18 @@ git push origin --tags -q
 
 echo Tee latest tag to VERSION
 # https://github.com/screwdriver-cd/gitversion/releases
-GIT_VERSION=/opt/sd/gitversion
+if [ "$SCREWDRIVER" == true ]; then
+  GIT_VERSION=/opt/sd/gitversion
+else
+  GIT_VERSION=/tmp/gitversion
+  if [ ! -f "$GIT_VERSION" ] ; then
+    echo Downloading gitversion
+    wget -q -O - https://github.com/screwdriver-cd/gitversion/releases/latest \
+      | egrep -o '/screwdriver-cd/gitversion/releases/download/v[0-9.]*/gitversion_linux_amd64' \
+      | wget --base=http://github.com/ -i - -O /tmp/gitversion
+    chmod +x $GIT_VERSION
+  fi
+fi
 
 echo Getting latest version
 $GIT_VERSION --prefix v show | tee VERSION
