3351fc12a52811e48c019e2827ff388124f0dca4
klu909 <55161078+klu909@users.noreply.github.com>
2022-09-19 17:34:05 -0700

    Update git-tag.sh

--- git-tag.sh
+++ git-tag.sh
@@ -11,7 +11,7 @@ else
   GIT_VERSION=/tmp/gitversion
   if [ ! -f "$GIT_VERSION" ] ; then
     echo Downloading gitversion
-    wget -q -O - https://github.com/screwdriver-cd/gitversion/releases/latest \
+    wget -q -O - https://api.github.com/repos/screwdriver-cd/gitversion/releases/latest \
       | egrep -o '/screwdriver-cd/gitversion/releases/download/v[0-9.]*/gitversion_linux_amd64' \
       | wget --base=http://github.com/ -i - -O /tmp/gitversion
     chmod +x $GIT_VERSION
